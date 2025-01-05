b44c1b14917a51be7280b52606d14d75ea69fe7e
Heedon Ham <98685150+phillyWork@users.noreply.github.com>
2025-01-05 15:47:58 +0900

    Upload Solution

---
+++ LeetCode/Easy/2769. Find the Maximum Achievable Number/2769. Find the Maximum Achievable Number.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+// increase or decrease t times
+// x - t = num + t
+
+class Solution {
+    func theMaximumAchievableX(_ num: Int, _ t: Int) -> Int {
+        return num + 2*t
+    }
+}

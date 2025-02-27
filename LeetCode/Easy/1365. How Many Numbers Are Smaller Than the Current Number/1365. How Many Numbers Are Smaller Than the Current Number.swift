6a4b642d014069fb77aeb971f038734d13119b07
Heedon Ham <98685150+phillyWork@users.noreply.github.com>
2025-02-27 17:33:43 +0900

    Update 1365. How Many Numbers Are Smaller Than the Current Number.swift

--- LeetCode/Easy/1365. How Many Numbers Are Smaller Than the Current Number/1365. How Many Numbers Are Smaller Than the Current Number.swift
+++ LeetCode/Easy/1365. How Many Numbers Are Smaller Than the Current Number/1365. How Many Numbers Are Smaller Than the Current Number.swift
@@ -1,6 +1,7 @@
 import Foundation
 
-// 
+// set current number as max
+// compare with each element
 
 class Solution {
     func smallerNumbersThanCurrent(_ nums: [Int]) -> [Int] {
@@ -19,6 +20,33 @@ class Solution {
             result.append(count)
         }
 
+        return result
+    }
+}
+
+// Improvement
+// O(n^2) --> O(nlogn)
+
+class Solution {
+    func smallerNumbersThanCurrent(_ nums: [Int]) -> [Int] {
+
+        var count = Array(repeating: 0, count: 101) // range of nums[i]: 0~100 --> array size: 101
+        var result = [Int](repeating: 0, count: nums.count)
+        
+        // count nums
+        for num in nums {
+            count[num] += 1
+        }
+        
+        // check accumulated sum, add up which is smaller than current num
+        for i in 1..<101 {
+            count[i] += count[i - 1]
+        }
+        
+        for i in 0..<nums.count {
+            result[i] = nums[i] == 0 ? 0 : count[nums[i] - 1]
+        }
+        
         return result
     }
 }
